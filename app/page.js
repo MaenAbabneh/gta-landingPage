ae7dc9b82d5ea2224a1c2bdca47e9a51edd3a985
Maen <maenababneh@outlook.com>
2025-09-21 17:34:24 +0300

    test

--- app/page.js
+++ app/page.js
@@ -6,9 +6,9 @@ import { ScrollToPlugin, ScrollTrigger } from "gsap/all";
 import { usePathname } from "next/navigation";
 import { useRef } from "react";
 
-import Hero from "@/components/sections/hero.js";
-import Jason from "@/components/sections/jason/index.js";
-import JasonIntro from "@/components/sections/jason-intro.js";
+import Hero from "@/components/sections/hero";
+import Jason from "@/components/sections/jason/index";
+import JasonIntro from "@/components/sections/jason-intro";
 import { BouncingArrow } from "@/components/svg";
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
