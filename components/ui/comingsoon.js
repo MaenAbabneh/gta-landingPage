58a1cc91f18d733c24412d05311305783b849b37
Maen <maenababneh@outlook.com>
2025-11-30 01:20:28 +0300

    refactor: enhance ComingSoon component with conditional rendering and improved layout structure

--- components/ui/comingsoon.js
+++ components/ui/comingsoon.js
@@ -1,46 +1,51 @@
 import Image from "next/image";
 import { PsIcon, XboxIcon } from "@/components/ui/svg";
 
-function ComingSoon({ refs = {} }) {
+function ComingSoon({ refs = {}, isHero = false }) {
   const { comingSoonRef, VIlogoRef, textRef, consolesRef } = refs;
   return (
     <div
       ref={comingSoonRef}
-      className="absolute z-[0] inset-0 w-full h-lvh  flex flex-col items-center justify-center"
+      className={` h-lvh w-full flex flex-col items-center justify-center ${isHero ? " absolute z-[0] inset-0" : "relative "} `}
     >
-      <div className="flex flex-col items-center justify-center h-full w-full md:gap-5">
-        <div
-          ref={VIlogoRef}
-          className="hidden-VI-Logo opacity-0 relative lg:mb-5"
-          style={{
-            visibility: "hidden",
-          }}
-        >
-          <Image
-            src="/images/logo.webp"
-            alt="Grand Theft Auto VI Logo"
-            fill
-            sizes="( max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="object-cover "
-            priority
-          />
+      <div
+        className={` flex flex-col items-center justify-center  ${isHero ? "md:gap-5 lg:gap-10" : ""}`}
+      >
+        <div className={`flex flex-col items-center justify-center   ${isHero ? "coming-soon-gap" : "gap-5 md:gap-15 "}`}>
+          <div ref={VIlogoRef} className="relative  w-[clamp(20vh,25vw,30vh)] h-[clamp(8vh,10vw,15vh)] md:h-[clamp(10vh,15vw,20vh)]">
+            <Image
+              src="/images/logo.webp"
+              alt="Grand Theft Auto VI Logo"
+              sizes="( max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="object-contain"
+              fill
+              priority
+              style={{
+                visibility: isHero ? "hidden" : "visible",
+              }}
+            />
+          </div>
+          <h3
+            ref={textRef}
+            className="coming-soon-text font-black uppercase gradient-text "
+          >
+            COMING
+            <br />
+            November 19
+            <br />
+            2026
+          </h3>
         </div>
-        <h3
-          ref={textRef}
-          className=" text-center text-[3rem] md:text-[5rem] lg:text-[5.4rem] xl:text-[6.3rem] font-black leading-10 md:leading-17 lg:leading-19 xl:leading-22 gradient-text "
-        >
-          COMING
-          <br />
-          MAY 26
-          <br />
-          2026
-        </h3>
+
         <div
           ref={consolesRef}
-          className="flex flex-row items-center justify-center gap-5  text-gta-white "
+          className="flex flex-row items-center justify-center gap-10 text-gta-white "
+          style={{
+            visibility: isHero ? "inherit" : "hidden",
+          }}
         >
-          <PsIcon className="max-w-16  md:min-w-25 xl:max-w-100 md:max-h-40 xl:max-h-100" />
-          <XboxIcon className="max-w-25  md:min-w-40 xl:max-w-170 md:max-h-40 xl:max-h-100" />
+          <PsIcon className="w-30" />
+          <XboxIcon className="w-60" />
         </div>
       </div>
     </div>
