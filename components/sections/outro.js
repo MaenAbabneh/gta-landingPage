30772a3a58d466cc3ba2b1404d234d6c317e4dd5
Maen <maenababneh@outlook.com>
2025-12-03 22:50:40 +0300

    refactor: clean up Outro component code and improve video class styling for better layout

--- components/sections/outro.js
+++ components/sections/outro.js
@@ -7,7 +7,6 @@ import { useLazyVideo } from "@/hooks/useLazyVideo";
 import { getAssetIds } from "@/constants/assest";
 import { useVideoAnimation } from "@/hooks/animation/useVideoAnimation";
 
-
 function Outro() {
   const sectionRef = useRef(null);
   const videoOverlayRef = useRef(null);
@@ -15,25 +14,37 @@ function Outro() {
   const canvasRef = useRef(null);
   const storytextRef = useRef(null);
 
-  const { desktop: outroDesktop, mobile: outroMobile } = getAssetIds("outro_dy82ms");
-  const { videoUrl: videoSrc, posterUrl, posterMobile } = useLazyVideo({ desktop: outroDesktop || "outro_dy82ms", mobile: outroMobile || "Outro_mobile_pdesiw" }, {
-    eager: true,
-  });
+  const { desktop: outroDesktop, mobile: outroMobile } =
+    getAssetIds("outro_dy82ms");
+  const {
+    videoUrl: videoSrc,
+    posterUrl,
+    posterMobile,
+  } = useLazyVideo(
+    {
+      desktop: outroDesktop || "outro_dy82ms",
+      mobile: outroMobile || "Outro_mobile_pdesiw",
+    },
+    {
+      eager: true,
+    }
+  );
+  
   useVideoAnimation(
-    { sectionRef, videoOverlayRef, videoRef, canvasRef , storytextRef },
+    { sectionRef, videoOverlayRef, videoRef, canvasRef, storytextRef },
     videoSrc,
     {
       videoStart: 0,
       videoEnd: 0.8,
       sectionPinEnd: "bottom top-1000",
       isOutro: true,
-    },
+    }
   );
 
   return (
     <div
       ref={sectionRef}
-      className="relative w-full h-lvh mt-[50vh] overflow-hidden"
+      className="relative w-full h-lvh overflow-hidden"
     >
       <div ref={videoOverlayRef} className="h-lvh">
         <AnimatedVideoSection
@@ -42,9 +53,9 @@ function Outro() {
           posterMobile={posterMobile}
           videoSrc={videoSrc}
           canvasRef={canvasRef}
-          videoClassName="object-cover [object-position:70%_center] md:[object-position:80%_center] xl:[object-position:90%_center]"
-          posterClassName="object-cover [object-position:70%_center] md:[object-position:80%_center] xl:[object-position:90%_center]"
-          canvasClassName="object-cover [object-position:70%_center] md:[object-position:80%_center] xl:[object-position:90%_center]"
+          videoClassName="object-cover [object-position:70%_center] md:[object-position:50%_center] xl:[object-position:90%_center]"
+          posterClassName="object-cover [object-position:70%_center] md:[object-position:50%_center] xl:[object-position:90%_center]"
+          canvasClassName="object-cover [object-position:70%_center] md:[object-position:50%_center] xl:[object-position:90%_center]"
           videoAlt="Video showing Jason Duval in various scenes"
           imgAlt="Poster image for video showing Jason Duval in various scenes"
         />
