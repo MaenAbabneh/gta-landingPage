15d4b12c86cec3c1a78e875de8f41a2d8dd47334
Maen <maenababneh@outlook.com>
2025-12-05 15:39:42 +0300

    feat: enhance sitemap generation by including dynamic routes from gtaData

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,9 @@
+import { gtaData } from "@/constants/Links"; 
+
 export default function sitemap() {
   const baseUrl = "https://gta-vi-landing.vercel.app";
 
-  return [
+  const routes = [
     {
       url: baseUrl,
       lastModified: new Date(),
@@ -9,4 +11,22 @@ export default function sitemap() {
       priority: 1.0,
     },
   ];
+
+
+  const allSections = Object.values(gtaData)
+    .flatMap((category) => category)
+    .filter((item) => item.href) 
+    .map((item) => item.href.replace(/^\/+|#+/g, "")) 
+    .filter((section) => section && !section.includes("/"));
+
+  allSections.forEach((slug) => {
+    routes.push({
+      url: `${baseUrl}/${slug}`,
+      lastModified: new Date(),
+      changeFrequency: "monthly", 
+      priority: 0.8, 
+    });
+  });
+
+  return routes;
 }
